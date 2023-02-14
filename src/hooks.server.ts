f8d59925501121792e3e84a365a71bd4c6784af8
gustavocadev <churaalaygustavo11@gmail.com>
2023-02-14 18:38:52 -0500

    feat: add custom handle

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,10 @@
 import { auth } from './lib/server/lucia';
 import { handleHooks } from '@lucia-auth/sveltekit';
+import type { Handle } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
 
-export const handle = handleHooks(auth);
+export const customHandle = (async ({ resolve, event }) => {
+	return resolve(event);
+}) satisfies Handle;
+
+export const handle = sequence(handleHooks(auth), customHandle);
