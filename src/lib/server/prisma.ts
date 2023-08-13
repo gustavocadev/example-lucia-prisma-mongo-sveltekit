8674c1eb991d22e30b462e50238b34f44484a9fa
gustavocadev <gustavocadev@gmail.com>
2023-08-13 11:19:56 -0500

    chore: add declare global

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,6 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 import { env } from '$env/dynamic/private';
 
+declare global {
+	// eslint-disable-next-line no-var
+	var __prisma: import('@prisma/client').PrismaClient;
+}
+
 const prisma = globalThis.__prisma || new PrismaClient();
 
 if (env.NODE_ENV === 'development') {
